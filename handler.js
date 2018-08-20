5729f49c9585f94417213f17b52394099e43a643
Danilo Salvio <danilo_salvio@live.com>
2018-08-20 01:17:03 -0300

    Creates read function.

--- handler.js
+++ handler.js
@@ -19,7 +19,7 @@ module.exports.create = async (event, context, callback) => {
       const dynamoDb = new AWS.DynamoDB.DocumentClient();
       await dynamoDb["put"](params).promise();
 
-      console.log("success")
+      // console.log("success")
 
       const response = {
         statusCode: 200,
@@ -34,4 +34,44 @@ module.exports.create = async (event, context, callback) => {
       console.log(e)
       callback(null);
     }
+}
+
+module.exports.read = async (event, context, callback) => {
+  console.log("logging parameters.");
+  // console.log(event.pathParameters);
+  const data = JSON.parse(event.body);
+  console.log(data);
+  console.log(data.id);
+
+  const params = {
+    TableName: tn,
+    Key: {
+      id: data.id
+    }
+  };
+
+  try {
+    const dynamoDb = new AWS.DynamoDB.DocumentClient();
+    const result = await dynamoDb["get"](params).promise();
+    if (result.Item) {
+      console.log(result.Item);
+
+      const r = JSON.stringify(result.Item);
+      // const r = result.Item;
+      const response = {
+          statusCode: 200,
+          body: r
+        };
+      // console.log("result is" + result.Item.id);
+
+      callback(null, response);
+    } else {
+      console.log("item not found");
+      callback(null, "Item not found");
+    }
+  } catch (e) {
+    console.log("error");
+    console.log(e.message);
+    return e;
+  }
 }
